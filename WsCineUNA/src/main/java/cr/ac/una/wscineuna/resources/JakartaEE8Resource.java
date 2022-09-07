a17aadd04065c6ab38fc7b55c697e384aca27193
Arauz <86898345+kjarj54@users.noreply.github.com>
2022-09-06 23:46:43 -0600

    Creacion del ws
    solo creacion no se modifico nada

---
+++ WsCineUNA/src/main/java/cr/ac/una/wscineuna/resources/JakartaEE8Resource.java
@@ -0,0 +1,20 @@
+package cr.ac.una.wscineuna.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author 
+ */
+@Path("rest")
+public class JakartaEE8Resource {
+    
+    @GET
+    public Response ping(){
+        return Response
+                .ok("ping")
+                .build();
+    }
+}
