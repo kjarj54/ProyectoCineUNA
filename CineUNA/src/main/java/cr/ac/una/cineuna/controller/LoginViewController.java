4c231f4d6da3e0632f8ab68150b3cfbca633b45d
emma278vargas <105175616+emma278vargas@users.noreply.github.com>
2022-09-20 12:51:26 -0600

    login

--- CineUNA/src/main/java/cr/ac/una/cineuna/controller/LoginViewController.java
+++ CineUNA/src/main/java/cr/ac/una/cineuna/controller/LoginViewController.java
@@ -4,9 +4,15 @@
  */
 package cr.ac.una.cineuna.controller;
 
+import com.jfoenix.controls.JFXButton;
+import cr.ac.una.cineuna.util.FlowController;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 
 /**
  * FXML Controller class
@@ -14,6 +20,19 @@ import javafx.fxml.Initializable;
  * @author Emanuel
  */
 public class LoginViewController extends Controller implements Initializable {
+
+    @FXML
+    private AnchorPane PantallaInicio;
+    @FXML
+    private ImageView imagen1;
+    @FXML
+    private ImageView imagen2;
+    @FXML
+    private JFXButton btnIngresar;
+    @FXML
+    private JFXButton btnResgistrarse;
+    @FXML
+    private JFXButton btnRecuperar;
 
     /**
      * Initializes the controller class.
@@ -26,6 +45,19 @@ public class LoginViewController extends Controller implements Initializable {
     @Override
     public void initialize() {
         
+    }
+
+    @FXML
+    private void OnActionbtnIngresar(ActionEvent event) {
+    }
+
+    @FXML
+    private void OnActionbtnResgistrarse(ActionEvent event) {
+        FlowController.getInstance().goViewInWindow("UsuariosView");
+    }
+
+    @FXML
+    private void OnActionbtnRecuperar(ActionEvent event) {
     }
     
 }
