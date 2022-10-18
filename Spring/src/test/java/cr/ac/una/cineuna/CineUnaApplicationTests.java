1a3820e48401ef16971b637033e35d9bb5e8dd69
ultra <64664984+ultrasin@users.noreply.github.com>
2022-10-18 14:24:32 -0600

    Activacion link email

---
+++ Spring/src/test/java/cr/ac/una/cineuna/CineUnaApplicationTests.java
@@ -0,0 +1,13 @@
+package cr.ac.una.cineuna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineUnaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
