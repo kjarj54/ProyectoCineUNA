1a3afb70c2f7f103ad23f28b4dcef11918b6d459
ultra <64664984+ultrasin@users.noreply.github.com>
2022-10-24 13:48:09 -0600

    arreglo del spring

---
+++ Spring/src/main/java/cr/ac/una/cineuna/activador/CineUnaApplication.java
@@ -0,0 +1,13 @@
+package cr.ac.una.cineuna.activador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineUnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineUnaApplication.class, args);
+	}
+
+}
