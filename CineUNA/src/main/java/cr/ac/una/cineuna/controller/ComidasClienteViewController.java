cc54f3c066bd859e430b72726665290b5dd6ec63
Arauz <86898345+kjarj54@users.noreply.github.com>
2022-10-27 22:26:35 -0600

    Cambios

--- CineUNA/src/main/java/cr/ac/una/cineuna/controller/ComidasClienteViewController.java
+++ CineUNA/src/main/java/cr/ac/una/cineuna/controller/ComidasClienteViewController.java
@@ -5,11 +5,17 @@
 package cr.ac.una.cineuna.controller;
 
 import com.jfoenix.controls.JFXButton;
+import cr.ac.una.cineuna.model.ProComidasDto;
+import cr.ac.una.cineuna.model.ProFacturasDto;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
@@ -23,29 +29,31 @@ public class ComidasClienteViewController extends Controller implements Initiali
     @FXML
     private Label txtComidas;
     @FXML
-    private TableView<?> tbvComidas;
+    private TableView<ProComidasDto> tbvComidas;
     @FXML
-    private TableColumn<?, ?> tbcNombre;
+    private TableColumn<ProComidasDto, String> tbcNombre;
     @FXML
-    private TableColumn<?, ?> tbcDescripcion;
+    private TableColumn<ProComidasDto, String> tbcDescripcion;
     @FXML
-    private TableColumn<?, ?> tbcPrecio;
+    private TableColumn<ProComidasDto, String> tbcPrecio;
     @FXML
-    private TableColumn<?, ?> tbcAgregar;
+    private TableColumn<ProComidasDto, String> tbcAgregar;
     @FXML
-    private TableView<?> tbvFactura;
+    private TableView<ProFacturasDto> tbvFactura;
     @FXML
-    private TableColumn<?, ?> tbcNombreFactura;
+    private TableColumn<ProFacturasDto, String> tbcNombreFactura;
     @FXML
-    private TableColumn<?, ?> tbcPrecioFactura;
+    private TableColumn<ProFacturasDto, String> tbcPrecioFactura;
     @FXML
-    private TableColumn<?, ?> tbcQuitar;
+    private TableColumn<ProFacturasDto, String> tbcQuitar;
     @FXML
     private Label txtTotal;
     @FXML
     private JFXButton btnPagar;
     @FXML
     private JFXButton btnSalir;
+    
+    
 
     /**
      * Initializes the controller class.
@@ -53,11 +61,67 @@ public class ComidasClienteViewController extends Controller implements Initiali
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        
+        tbcAgregar.setCellValueFactory((TableColumn.CellDataFeatures<ProComidasDto,Boolean >p)-> new SimpleObjectProperty(p.getValue() != null));
+        
+        tbcAgregar.setCellFactory((TableColumn<ProComidasDto, Boolean> p) -> new ButtonCellComida());
+        
+        tbcQuitar.setCellValueFactory((TableColumn.CellDataFeatures<ProFacturasDto,Boolean >p)-> new SimpleObjectProperty(p.getValue() != null));
+        
+        tbcQuitar.setCellFactory((TableColumn<ProFacturasDto, Boolean> p) -> new ButtonCellFactura());
     }    
 
     @Override
     public void initialize() {
         
+    }
+    
+    private class ButtonCellComida extends TableCell<ProComidasDto, Boolean> {
+
+        final Button cellButton = new Button();
+
+        ButtonCellComida() {
+            cellButton.setPrefWidth(500);
+            cellButton.getStyleClass().add("jfx-btnimg-tbveliminar");
+
+            cellButton.setOnAction((ActionEvent t) -> {
+                ProComidasDto com = (ProComidasDto) ButtonCellComida.this.getTableView().getItems().get(ButtonCellComida.this.getIndex());
+                tbvComidas.refresh();
+            });
+        }
+
+        @Override
+        protected void updateItem(Boolean t, boolean empty) {
+            super.updateItem(t, empty);
+            if (!empty) {
+                setGraphic(cellButton);
+            }
+        }
+    }
+    
+    private class ButtonCellFactura extends TableCell<ProFacturasDto, Boolean> {
+
+        final Button cellButton = new Button();
+
+        ButtonCellFactura() {
+            cellButton.setPrefWidth(500);
+            cellButton.getStyleClass().add("jfx-btnimg-tbveliminar");
+
+            cellButton.setOnAction((ActionEvent t) -> {
+                ProFacturasDto fac = (ProFacturasDto) ButtonCellFactura.this.getTableView().getItems().get(ButtonCellFactura.this.getIndex());
+                
+                tbvFactura.getItems().remove(fac);
+                tbvFactura.refresh();
+            });
+        }
+
+        @Override
+        protected void updateItem(Boolean t, boolean empty) {
+            super.updateItem(t, empty);
+            if (!empty) {
+                setGraphic(cellButton);
+            }
+        }
     }
     
 }
