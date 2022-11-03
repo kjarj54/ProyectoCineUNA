35dd03f46fb4d1abaf58bdf8593d054f020a8cdf
Arauz <86898345+kjarj54@users.noreply.github.com>
2022-11-02 19:58:51 -0600

    Idiomas
    El login ya funciona para que funcione en la parte de español hay que mejorar unas cosa nada mas para que funcione con el ingles y armar el bundle de ingles

--- CineUNA/src/main/java/cr/ac/una/cineuna/App.java
+++ CineUNA/src/main/java/cr/ac/una/cineuna/App.java
@@ -6,6 +6,8 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
 import javafx.scene.image.Image;
 
 /**
@@ -16,8 +18,13 @@ public class App extends Application {
     private static Scene scene;
 
     @Override
-    public void start(Stage stage) throws IOException {       
-        FlowController.getInstance().InitializeFlow(stage, null);
+    public void start(Stage stage) throws IOException {   
+        Locale localcurrent = Locale.getDefault();
+        Locale local = new Locale("es_ES");
+        
+        ResourceBundle bundle = ResourceBundle.getBundle("cr/ac/una/cineuna/resources/i18n/Bundle", local);
+        
+        FlowController.getInstance().InitializeFlow(stage, bundle);
         stage.getIcons().add(new Image(App.class.getResourceAsStream("/cr/ac/una/cineuna/resources/icono-pantalla.png")));
         stage.setTitle("UNA CINE");
         FlowController.getInstance().goViewInWindow("LoginView");
