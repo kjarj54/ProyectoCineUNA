d4c34408cfccf3d4bc9b354ac383553e84230ab6
Arauz <86898345+kjarj54@users.noreply.github.com>
2022-11-03 22:47:48 -0600

    Imagen

--- CineUNA/src/main/java/cr/ac/una/cineuna/controller/MantPeliculasViewController.java
+++ CineUNA/src/main/java/cr/ac/una/cineuna/controller/MantPeliculasViewController.java
@@ -2,6 +2,8 @@
 package cr.ac.una.cineuna.controller;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
@@ -9,6 +11,9 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
 
 /**
  * FXML Controller class
@@ -23,6 +28,18 @@ public class MantPeliculasViewController extends Controller implements Initializ
     private JFXButton btnCargarImagen;
     @FXML
     private TextArea txtAreaSinopsis;
+    @FXML
+    private ImageView imgPel;
+    @FXML
+    private JFXTextField txtUrlEspa;
+    @FXML
+    private JFXTextField txtUrlIngles;
+    @FXML
+    private JFXTextField txtNombrePel;
+    @FXML
+    private JFXButton btnGuardar;
+    @FXML
+    private JFXButton btnLimpiar;
 
     /**
      * Initializes the controller class.
@@ -39,6 +56,31 @@ public class MantPeliculasViewController extends Controller implements Initializ
 
     @FXML
     private void onActionCargarImagen(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Busqueda Imagen");
+        
+        //Facilita la busqueda escogiendo que aparescan jpg, pgn
+        fileChooser.getExtensionFilters().addAll( new FileChooser.ExtensionFilter("All Images", "*.*"), new FileChooser.ExtensionFilter("JPG", "*.jpg"), new FileChooser.ExtensionFilter("PNG", "*.png"));
+        
+        //toma la imagen
+        File imagFile = fileChooser.showOpenDialog(null);
+        
+        //comprueba y luego muestra la imagen
+        if(imagFile != null){
+            
+          Image image = new Image("file:"+imagFile.getAbsolutePath());
+          imgPel.setImage(image);
+          
+        }
+        
+    }
+
+    @FXML
+    private void onActionGuardar(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionLimpiar(ActionEvent event) {
     }
     
 }
