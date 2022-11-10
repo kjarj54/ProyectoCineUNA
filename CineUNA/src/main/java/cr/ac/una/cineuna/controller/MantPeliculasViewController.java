aab0a865113598fb0d25ac76c8afd41f87f7a316
Arauz <86898345+kjarj54@users.noreply.github.com>
2022-11-09 22:19:20 -0600

    MantPel

--- CineUNA/src/main/java/cr/ac/una/cineuna/controller/MantPeliculasViewController.java
+++ CineUNA/src/main/java/cr/ac/una/cineuna/controller/MantPeliculasViewController.java
@@ -1,19 +1,25 @@
-
 package cr.ac.una.cineuna.controller;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXDatePicker;
+import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
+import cr.ac.una.cineuna.model.ProPeliculasDto;
+import cr.ac.una.cineuna.util.Formato;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.TextArea;
@@ -52,6 +58,9 @@ public class MantPeliculasViewController extends Controller implements Initializ
     @FXML
     private JFXDatePicker dpFecha;
 
+    List<Node> requeridos = new ArrayList<>();
+    ProPeliculasDto proPeliculasDto;
+
     /**
      * Initializes the controller class.
      */
@@ -61,12 +70,85 @@ public class MantPeliculasViewController extends Controller implements Initializ
         menuTanda.getItems().add("S - En Sala");
         menuTanda.getItems().add("I - Inactiva");
         menuTanda.getItems().add("P - Proximamente");
-    }    
-    
+        txtNombrePel.setTextFormatter(Formato.getInstance().maxLengthFormat(30));
+        txtAreaSinopsis.setTextFormatter(Formato.getInstance().maxLengthFormat(500));
+        txtUrlEspa.setTextFormatter(Formato.getInstance().maxLengthFormat(200));
+        txtUrlIngles.setTextFormatter(Formato.getInstance().maxLengthFormat(200));
+        
+    }
+
+    public void indicarRequeridos() {
+        requeridos.clear();
+        requeridos.addAll(Arrays.asList(txtNombrePel, txtUrlEspa, txtUrlIngles));
+    }
+
+    public String validarRequeridos() {
+        Boolean validos = true;
+        String invalidos = "";
+        for (Node node : requeridos) {
+            if (node instanceof JFXTextField && !((JFXTextField) node).validate()) {
+                if (validos) {
+                    invalidos += ((JFXTextField) node).getPromptText();
+                } else {
+                    invalidos += "," + ((JFXTextField) node).getPromptText();
+                }
+                validos = false;
+            } else if (node instanceof JFXPasswordField && !((JFXPasswordField) node).validate()) {
+                if (validos) {
+                    invalidos += ((JFXPasswordField) node).getPromptText();
+                } else {
+                    invalidos += "," + ((JFXPasswordField) node).getPromptText();
+                }
+                validos = false;
+            } else if (node instanceof JFXDatePicker && ((JFXDatePicker) node).getValue() == null) {
+                if (validos) {
+                    invalidos += ((JFXDatePicker) node).getAccessibleText();
+                } else {
+                    invalidos += "," + ((JFXDatePicker) node).getAccessibleText();
+                }
+                validos = false;
+            } else if (node instanceof JFXComboBox && ((JFXComboBox) node).getSelectionModel().getSelectedIndex() < 0) {
+                if (validos) {
+                    invalidos += ((JFXComboBox) node).getPromptText();
+                } else {
+                    invalidos += "," + ((JFXComboBox) node).getPromptText();
+                }
+                validos = false;
+            }
+        }
+        if (validos) {
+            return "";
+        } else {
+            return "Campos requeridos o con problemas de formato [" + invalidos + "].";
+        }
+    }
+
+    public void bindPeliculas(Boolean nuevo) {
+        txtNombrePel.textProperty().bindBidirectional(proPeliculasDto.pelNombre);
+        txtAreaSinopsis.textProperty().bindBidirectional(proPeliculasDto.pelSynopsis);
+        txtUrlEspa.textProperty().bindBidirectional(proPeliculasDto.pelLink);
+        txtUrlIngles.textProperty().bindBidirectional(proPeliculasDto.pelLink);
+    }
+
+    public void unbindPeliculas() {
+        txtNombrePel.textProperty().unbindBidirectional(proPeliculasDto.pelNombre);
+        txtAreaSinopsis.textProperty().unbindBidirectional(proPeliculasDto.pelSynopsis);
+        txtUrlEspa.textProperty().unbindBidirectional(proPeliculasDto.pelLink);
+        txtUrlIngles.textProperty().unbindBidirectional(proPeliculasDto.pelLink);
+
+    }
+
+    public void nuevaPelicula() {
+        unbindPeliculas();
+        proPeliculasDto = new ProPeliculasDto();
+        bindPeliculas(true);
+        txtNombrePel.requestFocus();
+    }
+
     private Image ConvertToImage(byte[] data) {
         return new Image(new ByteArrayInputStream(data));
     }
-    
+
     private byte[] SaveImage(Image imagen) throws IOException {
         System.out.println(imagen);
         String image = imagen.getUrl().substring(6, imagen.getUrl().length());
@@ -76,37 +158,36 @@ public class MantPeliculasViewController extends Controller implements Initializ
         byte[] imageInBytes = IOUtils.toByteArray(fiStream);
         return imageInBytes;
     }
-    
+
     @Override
     public void initialize() {
-        
+
     }
 
     @FXML
     private void onActionCargarImagen(ActionEvent event) {
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Busqueda Imagen");
-        
+
         //Facilita la busqueda escogiendo que aparescan jpg, pgn
-        fileChooser.getExtensionFilters().addAll( new FileChooser.ExtensionFilter("All Images", "*.*"), new FileChooser.ExtensionFilter("JPG", "*.jpg"), new FileChooser.ExtensionFilter("PNG", "*.png"));
-        
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("All Images", "*.*"), new FileChooser.ExtensionFilter("JPG", "*.jpg"), new FileChooser.ExtensionFilter("PNG", "*.png"));
+
         //toma la imagen
         File imagFile = fileChooser.showOpenDialog(null);
-        
+
         //comprueba y luego muestra la imagen
-        if(imagFile != null){
-            
-          Image image = new Image("file:"+imagFile.getAbsolutePath());
-          imgPel.setImage(image);
-          
+        if (imagFile != null) {
+
+            Image image = new Image("file:" + imagFile.getAbsolutePath());
+            imgPel.setImage(image);
+
         }
-        
+
     }
 
     @FXML
     private void onActionGuardar(ActionEvent event) {
-        
-        
+
     }
 
     @FXML
--- CineUNA/src/main/java/cr/ac/una/cineuna/controller/UsuariosViewController.java
+++ CineUNA/src/main/java/cr/ac/una/cineuna/controller/UsuariosViewController.java
@@ -97,50 +97,6 @@ public class UsuariosViewController extends Controller implements Initializable 
 
     }
 
-    public void enviarCorreo() {
-        //provide recipient's email ID
-        String to = "diegocj92@gmail.com";
-        //provide sender's email ID
-        String from = "cineuna.smtp@gmail.com";
-        //provide Mailtrap's username
-        final String username = "cineuna.smtp@gmail.com";
-        //provide Mailtrap's password
-        final String password = "smtp.Cineuna";
-        //provide Mailtrap's host address
-        String host = "smtp.gmail.com";
-        //configure Mailtrap's SMTP server details
-        Properties props = new Properties();
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.host", host);
-        props.put("mail.smtp.port", "587");
-        //create the Session object
-        Session session = Session.getInstance(props,
-                new jakarta.mail.Authenticator() {
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(username, password);
-            }
-        });
-
-        try {
-            //create a MimeMessage object
-            Message message = new MimeMessage(session);
-            //set From email field
-            message.setFrom(new InternetAddress(from));
-            //set To email field
-            message.setRecipients(Message.RecipientType.TO,
-                    InternetAddress.parse(to));
-            //set email subject field
-            message.setSubject("Here comes Jakarta Mail!");
-            //set the content of the email message
-            message.setText("Just discovered that Jakarta Mail is fun and easy to use");
-            //send the email message
-            Transport.send(message);
-            System.out.println("Email Message Sent Successfully");
-        } catch (MessagingException e) {
-            throw new RuntimeException(e);
-        }
-    }
 
     public void indicarRequeridos() {
         requeridos.clear();
