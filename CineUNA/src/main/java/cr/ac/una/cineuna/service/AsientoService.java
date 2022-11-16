78d58d14fbef98f31157bcbe5dfe4b5e6756627b
ultra <64664984+ultrasin@users.noreply.github.com>
2022-11-15 19:47:15 -0600

    cambio salas

---
+++ CineUNA/src/main/java/cr/ac/una/cineuna/service/AsientoService.java
@@ -0,0 +1,15 @@
+/*
+ */
+package cr.ac.una.cineuna.service;
+
+import java.sql.Blob;
+
+/**
+ *
+ * @author BiblioPZ UNA
+ */
+public class AsientoService {
+    private Long id;
+    private String nombre;
+    private Blob imagen;
+}
